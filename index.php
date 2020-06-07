8fff8ca8658eb0414a9fbcd5f94ace047337d3ee
xirrow <45326541+xirrow@users.noreply.github.com>
2020-06-07 13:26:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 try
 
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=plantbas_zworrix_db', 'plantbas_zackworrix', 'samsara1440');
+    $pdo = new PDO('mysql:host=XXXX;dbname=XXXX', 'XXXX', 'XXXX');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
 }
